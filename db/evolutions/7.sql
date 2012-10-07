d9e266b58e61118a88c52ccfb6570b1a29e1b3fa
Regis Bamba <regis@rebugedge.com>
2012-10-08 00:13:35 +0100

    Add evolution

---
+++ db/evolutions/7.sql
@@ -0,0 +1,12 @@
+# --- !Ups
+ALTER TABLE thumbnail ALTER COLUMN document_id SET NOT NULL;
+
+ALTER TABLE thumbnail
+	ADD CONSTRAINT fk83434d34d0b1afe FOREIGN KEY (document_id)
+	REFERENCES "document" (id) MATCH SIMPLE ON UPDATE NO ACTION ON DELETE CASCADE;
+	
+# --- !Downs
+ALTER TABLE thumbnail
+	DROP CONSTRAINT fk83434d34d0b1afe;
+	
+ALTER TABLE thumbnail DROP COLUMN document_id;
